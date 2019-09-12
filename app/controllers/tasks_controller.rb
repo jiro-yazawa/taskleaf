6beea232c16b5cb93d6acbbe726f3c213f2c7cfe
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-09-12 22:16:04 +0900

    タスクのCSVインポート機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,6 +40,11 @@ class TasksController < ApplicationController
 
   def edit
   end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: 'タスクを追加しました。'
+  end
   
   def update
     @task.update!(task_params)
